a453a9b0d2aaf0b96b5f55a38a58b18a86c6349c
Tom Burns <tom.burns@jadedpixel.com>
2012-09-18 15:34:10 -0400

    When resizing if either dimension is 0, maintain aspect ratio

--- gd_image.go
+++ gd_image.go
@@ -17,6 +17,18 @@ func NewImage(width, height int) Image {
 }
 
 func (img *GdImage) NewResized(bounds Bounds) (*GdImage, error) {
+
+	if bounds == (Bounds{0, 0}) {
+		return nil, resampleError
+	}
+
+	if bounds.Width == 0 {
+		bounds.Width = img.image().width() * bounds.Height / img.image().height()
+	}
+	if bounds.Height == 0 {
+		bounds.Height = img.image().height() * bounds.Width / img.image().width()
+	}
+
 	resized := img.image().gdCopyResized(0, 0, 0, 0, bounds.Width, bounds.Height, img.image().width(), img.image().height())
 	if resized == nil {
 		return nil, resampleError
