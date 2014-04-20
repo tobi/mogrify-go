bbdb0205be74100b6175334922b727645a461458
Antoine Grondin <antoinegrondin@gmail.com>
2014-04-19 20:04:01 -0400

    Fix compilation of example.

--- example/resize.go
+++ example/resize.go
@@ -1,40 +1,33 @@
 package main
 
 import (
-  "os"
-  "fmt"
+	"fmt"
 	"github.com/tobi/mogrify-go"
+	"log"
+	"os"
 )
 
 func main() {
 
-  file, err := os.Open("../assets/image.jpg")
+	file, err := os.Open("../assets/image.jpg")
 
-  if err != nil {
-    fmt.Errorf("Error loading file: %s ", err)
-    os.Exit(1)
-    return
-  }
+	if err != nil {
+		log.Fatalf("Error loading file: %s ", err)
+	}
 
-  jpg := mogrify.DecodeJpg(file)
-  
-  if jpg == nil {
-    fmt.Println("could not load image")
-    os.Exit(1)
-    return
-  }
+	jpg := mogrify.DecodeJpeg(file)
+	if jpg == nil {
+		log.Fatalln("could not load image")
+	}
+	defer jpg.Destroy()
 
-  defer jpg.Destroy()
+	b := mogrify.Bounds{Width: 50, Height: 50}
 
-  resized, err := jpg.NewResized(50, 50)
+	resized, err := jpg.NewResized(b)
+	if err != nil {
+		log.Fatalf("failed to resize: %s", err)
+	}
+	defer resized.Destroy()
 
-  if err != nil {
-    fmt.Errorf("failed to resize: %s", err)
-    os.Exit(1)
-    return
-  } 
-
-  defer resized.Destroy()
-
-  fmt.Printf("Resized image from %s => %s \n", mogrify.Dimensions(jpg), mogrify.Dimensions(resized))
+	fmt.Printf("Resized image from %s => %s \n", mogrify.Dimensions(jpg), mogrify.Dimensions(resized))
 }
